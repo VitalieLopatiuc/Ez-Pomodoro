9f70ba1f97a9fdbef6f2efec858165bb32191224
Vitalie Lopatiuc <vlopatiuc@gmail.com>
2022-10-23 22:09:35 -0400

    First commit

---
+++ Ez_PomodoroApp.swift
@@ -0,0 +1,18 @@
+//
+//  Ez_PomodoroApp.swift
+//  Ez Pomodoro
+//
+//  Created by Vitalie Lopatiuc on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Ez_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
